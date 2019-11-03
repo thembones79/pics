777c4d0651a1f69fcc679c00834b52b2a706298d
thembones79 <michal.szumnarski@gmail.com>
2019-11-03 23:41:04 +0100

    refactored using async await

--- src/components/App.js
+++ src/components/App.js
@@ -2,25 +2,23 @@ import React from "react";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
-  onSearchSubmit(term) {
+  async onSearchSubmit(term) {
     console.log({ term });
 
     const url = new URL("https://api.unsplash.com/search/photos/");
     const params = { query: term };
     url.search = new URLSearchParams(params).toString();
 
-    fetch(url, {
+    const response = await fetch(url, {
       method: "get",
       headers: new Headers({
         Authorization: `Client-ID 0363124836c07cb3e02d4d7cc8ca6c82ac871117298db44d67fde1051e33c126`,
         "Content-Type": "application/json"
       })
-    })
-      .then(resp => resp.json())
-      .then(resp => {
-        const gdpData = resp.results;
-        console.log({ gdpData });
-      });
+    });
+
+    const images = await response.json();
+    console.log(images.results);
   }
 
   render() {
