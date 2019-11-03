27a97810734a1c16845fab15b544648968c17032
thembones79 <michal.szumnarski@gmail.com>
2019-11-03 23:27:15 +0100

    added fetch with a promise (to unsplash)

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,35 @@
 import React from "react";
 import SearchBar from "./SearchBar";
 
-const App = () => {
-  return (
-  <div className="ui container" style={{marginTop: "10px"}}>
-    <SearchBar />
-  </div>
-  );
-};
+class App extends React.Component {
+  onSearchSubmit(term) {
+    console.log({ term });
+
+    const url = new URL("https://api.unsplash.com/search/photos/");
+    const params = { query: term };
+    url.search = new URLSearchParams(params).toString();
+
+    fetch(url, {
+      method: "get",
+      headers: new Headers({
+        Authorization: `Client-ID 0363124836c07cb3e02d4d7cc8ca6c82ac871117298db44d67fde1051e33c126`,
+        "Content-Type": "application/json"
+      })
+    })
+      .then(resp => resp.json())
+      .then(resp => {
+        const gdpData = resp.results;
+        console.log({ gdpData });
+      });
+  }
+
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: "10px" }}>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+      </div>
+    );
+  }
+}
 
 export default App;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,10 +3,10 @@ import React from "react";
 class SearchBar extends React.Component {
   state = { term: "" };
 
- onFormSubmit=(event) => {
-   event.preventDefault();
-   console.log(this.state.term)
- }
+  onFormSubmit = event => {
+    event.preventDefault();
+    this.props.onSubmit(this.state.term);
+  };
 
   render() {
     return (
