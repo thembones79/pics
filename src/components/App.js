dbb756b8ed04ef287e2b318ff5e512b67717753a
thembones79 <michal.szumnarski@gmail.com>
2019-11-04 23:33:15 +0100

    fetch was successfull

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,22 @@
 import React from "react";
 import SearchBar from "./SearchBar";
-import fetchUnsplashDataArray from "./fetchUnsplashDataArray"
+import ImageList from "./ImageList";
+import fetchUnsplashDataArray from "../api/fetchUnsplashDataArray";
 
 class App extends React.Component {
+  state = { images: [] };
 
-  state = {images: []}
+  onSearchSubmit = async term => {
+    const images = await fetchUnsplashDataArray(term);
 
-
-
-async onSearchSubmit(term) {
-console.log(await fetchUnsplashDataArray(term))
-  }
+    this.setState({ images });
+  };
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        <ImageList images={this.state.images} />
       </div>
     );
   }
